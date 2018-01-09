ee16ded3aee8511fa776cb588a691c3e81d7e29c
ha-noh <7147407+ha-noh@users.noreply.github.com>
2018-01-09 04:31:59 -0500

    playlist.hide
    +initial hide does not return errors from race conditions
    -pc performance may lead to bad ux if hiding interferes with user

--- js/playlists.js
+++ js/playlists.js
@@ -1,13 +1,18 @@
 $(function(){
+
   //hide all playlists after page loads
-  $(".playlist").hide();
+  $("iframe").each(function(){
+    this.onload = function(){
+      $(this).hide();
+    };
+  });
 
   /*
    * Single function that detects any clicks on playlist synopses
-   *
+   * Finds the child of the button being clicked on.
    */
   $(".button").click(function(){
-    var playlist=$(this).find("iframe");
+    var playlist=$(this).siblings("iframe");
     playlist.toggle();
   });
 });

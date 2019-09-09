2dc7603be430e787920b5a73626fce45c5d067e5
Andrew <7147407+ha-noh@users.noreply.github.com>
2019-09-09 18:31:51 -0400

    gallery category tabs
    +functioning tabs in gallery.html

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,26 @@
 // load navbar
 $(document).ready(function() {
-	$("#nav-placeholder").load("html/navbar.html");
+	$('#nav-placeholder').load('html/navbar.html');
 });
 
+function openCategory(e, categoryName){
+	// if the function is trying to open the current category, return
+	if(document.querySelector('.displayed').classList.contains(categoryName.slice(1))){
+		console.log(`${categoryName} is already active`);
+		return;
+	}
+
+	// hide active gallery section by toggling .displayed and .hidden classes
+	let currentCategory = document.querySelector('.displayed');
+	currentCategory.classList.add('hidden');
+	currentCategory.classList.remove('displayed');
+
+	// remove the .active class from active category's button
+	document.querySelector('.active').classList.remove('active');
+
+	// open new category and add .active to its button's classes
+	let newCategory = document.querySelector(categoryName);
+	newCategory.classList.add('displayed');
+	newCategory.classList.remove('hidden');
+	e.currentTarget.className += ' active';
+}

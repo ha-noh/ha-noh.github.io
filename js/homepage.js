5dd38eacee241b6defba2d7de4c31469121f22c1
Andrew <7147407+ha-noh@users.noreply.github.com>
2020-01-07 16:58:59 -0500

    migrated inline js
    + added (currently non-functional) nightmode toggle to home page
    + added global css rule for <button> in _common.scss
    + moved inline js call openDesignCategory() to homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -1,6 +1,7 @@
 // replaces dummy nav with icons
 $(document).ready(function() {
 	$("#nav-placeholder").load("html/navbar.html");
+	document.querySelector('.panel-botleft a').addEventListener('click', openDesignCategory);
 });
 
 // opens the corresponding category if it's clicked through on the home page

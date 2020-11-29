a988327d0dfcaf473a862b10b00fbde151fd70da
Andrew <7147407+ha-noh@users.noreply.github.com>
2020-11-28 23:18:07 -0500

    Added new header to all pages
    + All pages now use the new header; currently the gallery forgoes the sticky styling due to its conflict with the gallery header

--- js/aboutme.js
+++ js/aboutme.js
@@ -10,4 +10,20 @@ window.addEventListener('DOMContentLoaded', () => {
 			details.children[0].innerText = 'Click for change list';
 		}
 	});
+
+	const stickyEle = document.querySelector('header');
+	
+	const observer = new IntersectionObserver(
+		([e]) => {
+			e.target.classList.toggle('isSticky', e.intersectionRatio < 1);
+			const nodes = e.target.children;
+			for(const node of nodes) {
+				node.classList.toggle('isSticky', e.intersectionRatio < 1);
+			}
+		},	
+		{threshold: [1]}
+	);
+
+	observer.observe(stickyEle);
 });
+
--- js/codeprojects.js
+++ js/codeprojects.js
@@ -1,5 +1,18 @@
 const codeprojectsjs = (function(){
-	// $(document).ready(function() {
-	// 	$("#nav-placeholder").load("html/navbar.html");
-	// });
+	window.addEventListener('DOMContentLoaded', e => {
+		const stickyEle = document.querySelector('header');
+	
+		const observer = new IntersectionObserver(
+			([e]) => {
+				e.target.classList.toggle('isSticky', e.intersectionRatio < 1);
+				const nodes = e.target.children;
+				for(const node of nodes) {
+					node.classList.toggle('isSticky', e.intersectionRatio < 1);
+				}
+			},	
+			{threshold: [1]}
+		);
+	
+		observer.observe(stickyEle);
+	});
 }());
